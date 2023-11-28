5acd8d8b93960c60e66372877112acea83f1fd8f
Utkalini123 <utkalinipriyadarshini18@gmail.com>
2023-11-28 20:48:57 +0530

    Adding New Module

---
+++ WEB TECH/p.js
@@ -0,0 +1,6 @@
+alert("Welcome to Heaven")
+var name1 = prompt("Enter your name :")
+var age1 = Number(prompt("Enter your age :"))
+var d = age1*365
+var r =(78-age1)*365
+alert("Your name is "+name1+" you lived "+d+" days and your remaining days are"+r)
